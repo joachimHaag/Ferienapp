d87bffbedf7b1decb0df322e3e098a4c9fa3e5e2
joachimHaag <93935845+joachimHaag@users.noreply.github.com>
2022-01-25 09:44:35 +0100

    local storage abgeschlossen

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { saveToLocal, loadFromLocal } from "./localStorage";
+
+function Settings() {
+  return (
+    <div>
+      <input name="Vorname" placeholder="Vorname" type={"text"} />
+      <input name="Nachname" placeholder="Vorname" type={"text"} />
+      <input name="Kind" placeholder="Kind" type={"text"} />
+      <input name="Mail" placeholder="Mail" type={"mail"} />
+    </div>
+  );
+}
